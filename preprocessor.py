b1e11dc3e4f5d0fe847c388113889c3f6ec49c9d
Birbal kumar <156226107+Birbal5040@users.noreply.github.com>
2025-03-14 07:25:08 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -1,4 +1,4 @@
-
+import re
 import pandas as pd
 
 def preprocess(data):
@@ -7,7 +7,7 @@ def preprocess(data):
     messages = re.split(pattern, data)[1:]
     dates = re.findall(pattern, data)
 
-    df = pd.DataFrame({'user_message': messages, 'message_date': dates})
+    #df = pd.DataFrame({'user_message': messages, 'message_date': dates})
     # convert message_date type
     df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ')
 
